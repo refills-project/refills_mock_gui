3067ad26c74a46380b130572b62f963e7d06bba3
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2018-03-16 16:56:46 +0100

    Wired the buttons.

--- scripts/mock_gui.py
+++ scripts/mock_gui.py
@@ -4,19 +4,39 @@ from PyQt4 import QtCore, QtGui
 from refills_mock_gui.layout import Ui_MainWindow
 import sys
 import rospy
+import actionlib
+import refills_msgs.msg
 
 class MockSmsMainWindow(Ui_MainWindow):
     def __init__(self):
         super(MockSmsMainWindow, self).__init__()
 
     def ros_setup(self):
-        
+        self.client = actionlib.SimpleActionClient('/scanning_action', refills_msgs.msg.ScanningAction)
+        if not self.client.wait_for_server(rospy.Duration(1)):
+            raise RuntimeError("Could not connect to action server of '/scanning_action'.")
+
         for loc_id in rospy.get_param("/mock_gui/loc_ids"):
             self.listWidget.addItem(QtGui.QListWidgetItem(loc_id))
         self.listWidget.itemClicked.connect(self.set_target_loc)
 
+        self.execButton.clicked.connect(self.start_scan)
+        self.cancelButton.clicked.connect(self.cancel_scan)
+
     def set_target_loc(self, item):
-        self.target_loc = str(item.text)
+        self.target_loc = str(item.text())
+
+    def start_scan(self):
+        goal = refills_msgs.msg.ScanningGoal(type=refills_msgs.msg.ScanningGoal.COMPLETE_SCAN, loc_id=[self.target_loc])
+        self.client.send_goal(goal, feedback_cb=self.feedback_cb)
+        self.client.wait_for_result()
+
+    def cancel_scan(self):
+        # TODO: complete me
+
+    def feedback_cb(self, msg):
+        # TODO: complete me
+        pass
 
 
 if __name__ == "__main__":
